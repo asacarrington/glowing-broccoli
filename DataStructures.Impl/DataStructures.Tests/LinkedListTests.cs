3c3e607dec1f49f359d465c934b437a31555e290
acarrington <asa.carrington@kpmg.co.uk>
2016-03-03 18:28:08 +0000

    Initial Commit

---
+++ DataStructures.Impl/DataStructures.Tests/LinkedListTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructures.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Tests
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        private readonly LinkedList _linkedList;
+        public LinkedListTests()
+        {
+            _linkedList = new LinkedList();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            this._linkedList.Add(new object());
+            Assert.AreEqual(this._linkedList.getCount(), 1);
+        }
+    }
+}
