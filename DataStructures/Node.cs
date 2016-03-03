ffcf70983446d41356acf450cbee09cc3a41f30a
acarrington <asa.carrington@kpmg.co.uk>
2016-03-03 15:57:47 +0000

    Inital Commit

---
+++ DataStructures/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Node
+    {
+        public Node Next;
+
+        public object Data;
+
+        public Node(object data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+
+    }
+}
